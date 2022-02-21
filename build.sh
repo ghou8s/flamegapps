add01aff182e131517a351ff237d53beef6484e3
ghou8s <ghou8s@gmail.com>
2022-02-21 08:54:11 +0000

    flamegapps: unofficial
    Signed-off-by: ghou8s <ghou8s@gmail.com>
    Change-Id: I68224b5d266bf78d62e89a51157cf21dc720b902

--- build.sh
+++ build.sh
@@ -93,7 +93,7 @@ make_gapps() {
 }
 
 create_zip() {
-  FILE_NAME="FlameGApps-$ANDROID-$EDITION-$ARCH-$ZIP_DATE"
+  FILE_NAME="FlameGApps-$ANDROID-$EDITION-$ARCH-$ZIP_DATE-unofficial"
   echo "--> Creating $FILE_NAME-UNSIGNED.zip"
   cd $ZIP_DIR
   zip -9 -r $FILE_NAME-UNSIGNED.zip .
